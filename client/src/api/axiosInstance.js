12efdcd7a7ede7692a9756a6734f0496415f235d
Anishk Chhamalwan <anishk.chhamalwan@ibm.com>
2025-11-27 23:29:47 +0530

    AxiosInstance removed local host 5000

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();  
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL 
 });
 
 api.interceptors.request.use((config) => {
